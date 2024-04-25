b8b9dc78862b443ed3190d99b999711dde80848d
itsreverence <158013050+itsreverence@users.noreply.github.com>
2024-04-25 00:48:30 -0500

    Reformat Arduino Sketch

--- Weather-Warning.ino
+++ Weather-Warning.ino
@@ -2,23 +2,26 @@
 #include <HTTPClient.h>
 #include <DHTesp.h>
 #include <IPAddress.h>
+#include "arduino_secrets.h"
 
-String lastOctet = "111" // last octet of local ip address of device hosting site
-const char* ssid = SECRET_SSID; // name of network to connect ESP to
-const char* password = SECRET_PASSWORD; // password of network to connect ESP to
+// Define variables for network
+String lastOctet = "111";
+const char* ssid = SECRET_SSID;
+const char* password = SECRET_PASSWORD;
 
-String serverName; // url where the data will get posted at
+// Define variables for database
+String serverName;
+String apiKeyValue = "mF8d0cge2";
+String sensorName = "DHT11";
+String sensorLocation = "Classroom";
 
-String apiKeyValue = "mF8d0cge2"; // database api key for validation
-String sensorName = "DHT11"; // sensor name to display
-String sensorLocation = "Classroom"; // location of station to display
+// Define variables for sensor
+DHTesp dht;
+int dhtPin = 13;
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
 
-DHTesp dht; // sensor object
-int dhtPin = 13; // sensor pin
-
-unsigned long lastTime = 0; // flag for last time data was sent
-unsigned long timerDelay = 30000; // delay between sending of data
-
+// Connect to network
 void setup() {
   Serial.begin(115200);
 
@@ -32,35 +35,37 @@ void setup() {
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
   
-  String prefixString = "http://";
-  uint8_t firstOctet = WiFi.localIP()[0];
-  uint8_t secondOctet = WiFi.localIP()[1];
-  uint8_t thirdOctet = WiFi.localIP()[2];
-  String suffixString = "/esp-post-data.php";
-  String completeUrl = prefixString  + String(firstOctet) + "." + String(secondOctet) + "." + String(thirdOctet) + "." + fourthOctet + suffixString;
-  serverName = completeUrl;
+  // Define address of site to post data to
+  serverName = "http://" + String(WiFi.localIP()[0]) + "." + String(WiFi.localIP()[1]) + "." + String(WiFi.localIP()[2]) + "." + lastOctet + "/esp-post-data.php";
 
-  Serial.println("Timer set to " + String(timerDelay / 1000) + " seconds (timerDelay variable), it will take 30 seconds before publishing the first reading.");
+  // Notify user of the current delay between posting of data
+  Serial.println("Timer set to " + String(timerDelay / 1000) + " seconds, it will take 30 seconds before publishing the first reading.");
 
+  // Initialize sensor to start collecting data
   dht.setup(dhtPin, DHTesp::DHT11);
 }
 
 void loop() {
+  // Check if it is time to send data again
   if ((millis() - lastTime) > timerDelay) {
+    // Check network connection and define wifi/http client variables
     if (WiFi.status() == WL_CONNECTED) {
       WiFiClient client;
       HTTPClient http;
 
+
 flag:
+      // Retrieve temperature and humidity values from sensor
       TempAndHumidity newValues = dht.getTempAndHumidity();
       if (dht.getStatus() != 0) {
         goto flag;
       }
 
+      // Prepare http client to send data
       http.begin(client, serverName);
-
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
+      // Prepare and log sensor data for post
       newValues.temperature = newValues.temperature * 9/5 + 32;
       String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
                                + "&location=" + sensorLocation + "&value1=" + newValues.temperature
@@ -68,8 +73,10 @@ flag:
       Serial.print("httpRequestData: ");
       Serial.println(httpRequestData);
 
+      // Post sensor data to site
       int httpResponseCode = http.POST(httpRequestData);
 
+      // Log the response code of the post
       if (httpResponseCode > 0) {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
@@ -77,10 +84,13 @@ flag:
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
       }
+      // Stop the http client
       http.end();
     } else {
+      // Log that connection got dropped
       Serial.println("WiFi Disconnected");
     }
+    // Set new value for last time data was sent
     lastTime = millis();
   }
 }
